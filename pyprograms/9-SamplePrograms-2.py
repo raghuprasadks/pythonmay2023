86e5cbf745e4ef2a6ee163e238c935031ad76b60
raghuprasadks <prasadraghuks@gmail.com>
2023-05-23 19:41:41 +0530

    updated

--- pyprograms/9-SamplePrograms-2.py
+++ pyprograms/9-SamplePrograms-2.py
@@ -30,9 +30,13 @@ ipldashboard=[]
 header = ["name","team","matches","runs","wickets"]
 player1 = ["virat","rcb",14,500,0]
 player2 = ["rohit","mi",14,350,0]
+player3 = ["jadeja","csk",14,250,10]
+
 ipldashboard.append(header)
 ipldashboard.append(player1)
 ipldashboard.append(player2)
+ipldashboard.append(player3)
+
 print("ipldashboard 2023")
 print(ipldashboard)
 
@@ -44,5 +48,33 @@ print("name and runs")
 for player in ipldashboard:
     print(player[0],player[3])
 
+
+print("total runs")
+'''
+totalruns=0
+for player in ipldashboard:
+    #totalruns = totalruns + player[2]
+
+#print("total runs ",totalruns)
+'''
+totalruns = 0
+totalwickets=0
+for rowindex in range(len(ipldashboard)):
+    if (rowindex==0):
+        continue
+    player = ipldashboard[rowindex]
+    totalruns = totalruns + player[3]
+    totalwickets = totalwickets + player[4]
+print("total runs ",totalruns)
+print("total wickets ",totalwickets)
+
+
+'''    
+for i in range(4):
+    print(i)
+'''
+
+
+#print(ipldashboard[1])
 
 
