d3fa0831b90ac6007a36c0de341917f38219d68a
raghuprasadks <prasadraghuks@gmail.com>
2023-05-26 19:42:32 +0530

    updated

--- pyprograms/10-SamplePrograms-3.py
+++ pyprograms/10-SamplePrograms-3.py
@@ -21,7 +21,7 @@ reverse(num)
 return the reverse of the number
 for example reverse(123)
 321
-"""
+
 def reverse(num):
     reverse=0
     while (num>0) :
@@ -38,4 +38,53 @@ if (n==rev):
     print(n, " is a palindrome")
 else:
     print(n, " is not a palindrome")
+"""
 
+octal = 512
+print("The decimal value of",octal, " is",to_decimal(octal))
+
+
+def to_decimal(oct_num):
+    sum=0
+    i=0
+    base=8
+    while (oct_num>0) :
+       reminder=oct_num%10
+       sum=sum+reminder*base**i
+       oct_num=oct_num//10
+       i=i+1
+    return sum
+
+number=to_decimal(111)
+print("Decimal number is :",number)
+
+
+#list_of_numbers=[]
+dict_of_numbers={}
+def has_hoagie(num):
+    temp_key=0
+    while(num>0):
+        rem=num%10
+        print(rem)
+        num=num//10
+#        list_of_numbers.append(rem)
+        dict_of_numbers[temp_key]=rem
+        temp_key+=1
+    if (len(dict_of_numbers)>2):
+        '''
+        prev_element=0
+        for i in range(1,len(list_of_numbers)-1):
+            next_element=i+1
+            if list_of_numbers[prev_element] == list_of_numbers[next_element] :
+                print("It has hoagie")
+            prev_element=prev_element+1
+        '''
+        prev_element=0
+        for i in range(1,len(dict_of_numbers)-1):
+            next_element=i+1
+            if dict_of_numbers[prev_element] == dict_of_numbers[next_element] :
+                print("It has hoagie")
+            prev_element=prev_element+1
+has_hoagie(1235)
+
+has_hoagie(737)
