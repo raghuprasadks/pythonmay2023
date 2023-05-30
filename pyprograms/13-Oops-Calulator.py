f8b363de56aa05d52cd34808a86971a2b42ac895
raghuprasadks <prasadraghuks@gmail.com>
2023-05-30 19:09:40 +0530

    oops

--- pyprograms/13-Oops-Calulator.py
+++ pyprograms/13-Oops-Calulator.py
@@ -23,9 +23,15 @@ class Calculator():
         self.result = self.num1 * self.num2
         return self.result
 
+    def info(self):
+        print("first number is ",self.num1)
+        print("second number is ", self.num2)
+
+
 #myCalculator1 object
 myCalculator1 = Calculator()
 result = myCalculator1.add(10,20)
+myCalculator1.info()
 print("result of addition ",result)
 
 result = myCalculator1.multiply(2,20)
