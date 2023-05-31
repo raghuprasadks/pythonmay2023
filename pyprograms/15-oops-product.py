2a45fec1c6f3b5a0eefabd963ed12924bff26096
raghuprasadks <prasadraghuks@gmail.com>
2023-05-31 19:17:23 +0530

    updated

--- pyprograms/15-oops-product.py
+++ pyprograms/15-oops-product.py
@@ -42,3 +42,49 @@ print("product 2 info",info)
 print("product 2 suppier ",product2.supplier)
 
 
+"""
+Constructor - At time of object creation only initilize the
+properties
+It is special method
+"""
+
+class Product():
+    #Member variables
+    code=-1
+    name=""
+    desc=""
+    supplier=""
+    price=-1
+
+    """
+    Constructor
+    """
+    def __init__(self,_code,_name,_desc,_supplier,_price):
+        self.code = _code
+        self.name = _name
+        self.desc = _desc
+        self.supplier =_supplier
+        self.price =_price
+
+
+    def setCode(self,_code):
+        self.code =_code
+
+    def getCode(self):
+        return self.code
+
+    def setName(self, _name):
+        self.name = _name
+
+    def getName(self):
+        return self.name
+
+    def info(self):
+        return "Code : ",self.code," Name : ",self.name
+
+
+
+product3 =Product(3,"TV","smart TC","sony",80000)
+info= product3.info()
+print("information ",info)
+#product4 = Product()
