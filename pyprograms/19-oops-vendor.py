09504d20ebc8234340b8445b283c531c96abfa52
raghuprasadks <prasadraghuks@gmail.com>
2023-06-03 19:09:34 +0530

    updated

---
+++ pyprograms/19-oops-vendor.py
@@ -0,0 +1,147 @@
+import random
+
+
+class Vendor:
+
+    def __init__(self, name):
+        '''
+            In this class, self refers to Vendor objects
+
+            name: str
+            vendor_id: random int in the range (999, 999999)
+        '''
+        self.name = name
+        self.vendor_id = random.randint(999, 999999)
+
+    def install(self):
+        '''
+            Creates and initializes (instantiate) an instance of VendingMachine
+        '''
+        return VendingMachine()
+
+    def restock(self, machine, item, amount):
+        '''
+            machine: VendingMachine
+            item: int
+            amount : int/float
+
+            Call _restock for the given VendingMachine object
+        '''
+        return machine._restock(item, amount)
+
+
+class VendingMachine:
+    '''
+        In this class, self refers to VendingMachine objects
+
+        >>> john_vendor = Vendor('John Doe')
+        >>> west_machine = john_vendor.install()
+        >>> west_machine.getStock
+        {156: [1.5, 3], 254: [2.0, 3], 384: [2.5, 3], 879: [3.0, 3]}
+        >>> john_vendor.restock(west_machine, 215, 9)
+        'Invalid item'
+        >>> west_machine.isStocked
+        True
+        >>> john_vendor.restock(west_machine,156, 1)
+        'Current item stock: 4'
+        >>> west_machine.getStock
+        {156: [1.5, 4], 254: [2.0, 3], 384: [2.5, 3], 879: [3.0, 3]}
+        >>> west_machine.purchase(156)
+        'Please deposit $1.5'
+        >>> west_machine.purchase(156,2)
+        'Please deposit $3.0'
+        >>> west_machine.purchase(156,23)
+        'Current 156 stock: 4, try again'
+        >>> west_machine.deposit(3)
+        'Balance: $3.0'
+        >>> west_machine.purchase(156,3)
+        'Please deposit $1.5'
+        >>> west_machine.purchase(156)
+        'Item dispensed, take your $1.5 back'
+        >>> west_machine.getStock
+        {156: [1.5, 3], 254: [2.0, 3], 384: [2.5, 3], 879: [3.0, 3]}
+        >>> west_machine.deposit(300)
+        'Balance: $300.0'
+        >>> west_machine.purchase(876)
+        'Invalid item'
+        >>> west_machine.purchase(384,3)
+        'Item dispensed, take your $292.5 back'
+        >>> west_machine.purchase(156,10)
+        'Current 156 stock: 3, try again'
+        >>> west_machine.purchase(156,3)
+        'Please deposit $4.5'
+        >>> west_machine.deposit(4.5)
+        'Balance: $4.5'
+        >>> west_machine.purchase(156,3)
+        'Item dispensed'
+        >>> west_machine.getStock
+        {156: [1.5, 0], 254: [2.0, 3], 384: [2.5, 0], 879: [3.0, 3]}
+        >>> west_machine.purchase(156)
+        'Item out of stock'
+        >>> west_machine.deposit(6)
+        'Balance: $6.0'
+        >>> west_machine.purchase(254,3)
+        'Item dispensed'
+        >>> west_machine.deposit(9)
+        'Balance: $9.0'
+        >>> west_machine.purchase(879,3)
+        'Item dispensed'
+        >>> west_machine.isStocked
+        False
+        >>> west_machine.deposit(5)
+        'Machine out of stock. Take your $5.0 back'
+        >>> west_machine.purchase(156,2)
+        'Machine out of stock'
+        >>> west_machine.purchase(665,2)
+        'Invalid item'
+        >>> east_machine = john_vendor.install()
+        >>> west_machine.getStock
+        {156: [1.5, 0], 254: [2.0, 0], 384: [2.5, 0], 879: [3.0, 0]}
+        >>> east_machine.getStock
+        {156: [1.5, 3], 254: [2.0, 3], 384: [2.5, 3], 879: [3.0, 3]}
+        >>> east_machine.deposit(10)
+        'Balance: $10.0'
+        >>> east_machine.cancelTransaction()
+        'Take your $10.0 back'
+        >>> east_machine.purchase(156)
+        'Please deposit $1.5'
+        >>> east_machine.cancelTransaction()
+    '''
+
+    def __init__(self):
+        # --- YOUR CODE STARTS HERE
+        #pass
+        self.vending={156: [1.5, 3], 254: [2.0, 3], 384: [2.5, 3], 879: [3.0, 3]}
+
+
+    def purchase(self, item, qty=1):
+        # --- YOUR CODE STARTS HERE
+        pass
+
+    def deposit(self, amount):
+        # --- YOUR CODE STARTS HERE
+        pass
+
+    def _restock(self, item, stock):
+        # --- YOUR CODE STARTS HERE
+        pass
+
+    # --- YOUR CODE STARTS HERE
+    def isStocked(self):
+        pass
+
+    # --- YOUR CODE STARTS HERE
+    def getStock(self):
+        #pass
+        return self.vending
+
+    def cancelTransaction(self):
+        # --- YOUR CODE STARTS HERE
+        pass
+
+"""
+Testing
+"""
+john_vendor = Vendor('John Doe')
+west_machine = john_vendor.install()
+print(west_machine.getStock())
