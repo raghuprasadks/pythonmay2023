7f1e6b574d63254661eac815ca9da22848636896
raghuprasadks <prasadraghuks@gmail.com>
2023-06-03 19:52:30 +0530

    updated

--- pyprograms/18-oops-pantry.py
+++ pyprograms/18-oops-pantry.py
@@ -32,6 +32,16 @@ class Pantry():
         else:
             print (f"You donot have  {item} ")
 
+    def tranfer(self,other_pantry, item:str):
+        if item in self.items.keys():
+            qty = other_pantry.items[item]
+            currentqty=self.items[item]
+            totalqty = currentqty + qty
+            self.items[item] = totalqty
+        else:
+            self.items[item] = other_pantry.items[item]
+        del other_pantry.items[item]
+
 sara_pantry = Pantry()
 print(sara_pantry)
 sara_pantry.stock_pantry('Bread', 2)
@@ -50,4 +60,11 @@ sara_pantry.get_item('Lettuce', .5)
 sara_pantry.get_item('dal', .5)
 
 ben_pantry = Pantry()
-print("Ben pantry ",ben_pantry)
+print("Ben pantry """,ben_pantry)
+ben_pantry.stock_pantry("Cereal",2)
+ben_pantry.stock_pantry("Noodles",5)
+print("ben pantry",ben_pantry)
+sara_pantry.tranfer(ben_pantry,"Noodles")
+print("sara pantry",sara_pantry)
+print("after tranfer :ben pantry",ben_pantry)
+
